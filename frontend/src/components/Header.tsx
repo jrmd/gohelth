0b9622e902e4a05a965b6efb92b2b800baf83310
Jerome T. Duncan <hello@itsjero.me>
2023-10-14 12:04:31 +0100

    refactor: tidy up

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,16 +9,6 @@ export function Header() {
 	}
 
 	return (
-		// <header>
-		// 	{user ? user.email : null}
-		// 	<nav>
-		// 		<a href="/" class={url == '/' && 'active'}>
-		// 			Home
-		// 		</a>
-		// 	</nav>
-		// </header>
-
-
 	<header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur">
 		<div className="container flex h-14 items-center">
 			<div className="mr-4 flex">
